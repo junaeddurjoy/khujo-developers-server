048eb240abebff53939b4891159eec0368d74ffb
junaeddurjoy <junaed.durjoy@gmail.com>
2023-11-07 14:04:27 +0600

    information updated

--- index.js
+++ index.js
@@ -72,11 +72,11 @@ async function run() {
       res.send(result);
     })
 
-    // delete applications
-    app.delete('/applications/:id', async (req, res) => {
+    // delete jobs
+    app.delete('/jobs/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
-      const result = await applyCollection.deleteOne(query);
+      const result = await jobCollection.deleteOne(query);
       res.send(result)
     });
 
